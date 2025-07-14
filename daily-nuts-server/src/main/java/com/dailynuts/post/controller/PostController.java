b58763e552633cbe88501ac8d149ab5f06300c1b
rlagusalsb <kkhm0929@gmail.com>
2025-07-14 23:21:50 +0900

    feat: PostController 구현

--- daily-nuts-server/src/main/java/com/dailynuts/post/controller/PostController.java
+++ daily-nuts-server/src/main/java/com/dailynuts/post/controller/PostController.java
@@ -1,8 +1,53 @@
 package com.dailynuts.post.controller;
 
-import org.springframework.stereotype.Controller;
+import com.dailynuts.post.dto.PostRequestDto;
+import com.dailynuts.post.dto.PostResponseDto;
+import com.dailynuts.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostResponseDto> createPost(
+            @RequestBody PostRequestDto requestDto)
+    {
+        PostResponseDto responseDto = postService.createPost(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDto> getPost(@PathVariable Long id) {
+        PostResponseDto responseDto = postService.getPost(id);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostResponseDto>> getAllPosts() {
+        List<PostResponseDto> responseDtoList = postService.getAllPosts();
+        return ResponseEntity.ok(responseDtoList);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostResponseDto> updatePost(
+            @PathVariable Long id, @RequestBody PostRequestDto requestDto)
+    {
+        PostResponseDto responseDto = postService.updatePost(id, requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+    }
 }
