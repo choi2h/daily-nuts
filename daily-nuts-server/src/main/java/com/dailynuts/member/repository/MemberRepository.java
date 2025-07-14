05758ef24400ac5c2f4c119c1d680d72e449c6f1
52kirai52 <52kirai52@gmail.com>
2025-07-14 13:44:33 +0900

    feat: MemberRepository 생성 및 초기 구현

---
+++ daily-nuts-server/src/main/java/com/dailynuts/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.dailynuts.member.repository;
+
+import com.dailynuts.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
