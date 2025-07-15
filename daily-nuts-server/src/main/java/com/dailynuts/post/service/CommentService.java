3a215e47ab5bcebd96273109773e7591648cec50
kimsongie12 <song3078@naver.com>
2025-07-15 16:27:56 +0900

    feat:CommentService구현

---
+++ daily-nuts-server/src/main/java/com/dailynuts/post/service/CommentService.java
@@ -0,0 +1,17 @@
+package com.dailynuts.post.service;
+
+import com.dailynuts.post.dto.CommentRequest;
+import com.dailynuts.post.dto.CommentResponse;
+
+import java.util.List;
+
+public interface CommentService {
+    //댓글 등록
+    CommentResponse createComment(Long postId, Long memberId, String writer, CommentRequest request);
+
+    //대댓글
+    CommentResponse createReplyToComment(Long postId, String parentCommentId, Long memberId, String writer, CommentRequest request);
+
+    //조회
+    List<CommentResponse> getCommentsByPost(Long postId);
+}
