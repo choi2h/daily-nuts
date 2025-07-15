71193b71a0634cd3fba10e497741644e971cd9be
rlagusalsb <kkhm0929@gmail.com>
2025-07-15 17:15:06 +0900

    refactor: 게시글 API 테스트 가능하도록 리팩토링

--- daily-nuts-server/src/main/java/com/dailynuts/post/service/PostServiceImpl.java
+++ daily-nuts-server/src/main/java/com/dailynuts/post/service/PostServiceImpl.java
@@ -3,10 +3,12 @@ package com.dailynuts.post.service;
 import com.dailynuts.common.exception.CustomErrorCode;
 import com.dailynuts.common.exception.CustomException;
 import com.dailynuts.member.entity.Member;
+import com.dailynuts.member.repository.MemberRepository;
 import com.dailynuts.post.dto.PostRequestDto;
 import com.dailynuts.post.dto.PostResponseDto;
 import com.dailynuts.post.entity.Category;
 import com.dailynuts.post.entity.Post;
+import com.dailynuts.post.repository.CategoryRepository;
 import com.dailynuts.post.repository.PostRepository;
 import com.dailynuts.post.service.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +23,18 @@ public class PostServiceImpl implements PostService{
 
     private final PostRepository postRepository;
     private final PostMapper postMapper;
+    private final MemberRepository memberRepository;
+    private final CategoryRepository categoryRepository;
 
     @Override
     @Transactional
     public PostResponseDto createPost(PostRequestDto request) {
-        //테스트용 더미 객체
-        Member member = new Member();
-        Category category = new Category();
+        //테스트용 멤버
+        Member member = memberRepository.findById(1L)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.MEMBER_NOT_EXIST));
+
+        //테스트용 카테고리
+        Category category = categoryRepository.findById(request.getCategoryId()).get();
 
         Post post = postMapper.getPostEntity(request, member, category);
         Post saved = postRepository.save(post);
@@ -56,8 +63,8 @@ public class PostServiceImpl implements PostService{
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new CustomException(CustomErrorCode.POST_NOT_FOUND));
 
-        //테스트용 더미 객체
-        Category category = new Category();
+        //테스트용 카테고리
+        Category category = categoryRepository.findById(request.getCategoryId()).get();
 
         post.update(request.getTitle(), request.getContent(), category);
         return postMapper.getPostResponseDto(post);
