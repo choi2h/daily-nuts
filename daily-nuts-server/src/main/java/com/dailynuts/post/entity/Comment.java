a408c127e9834c60a70fe338dfbeb74f41b25f7f
kimsongie12 <song3078@naver.com>
2025-07-16 15:18:18 +0900

    feat:Comment 타입변경
    id,CommentParentId->ObjectId변경

--- daily-nuts-server/src/main/java/com/dailynuts/post/entity/Comment.java
+++ daily-nuts-server/src/main/java/com/dailynuts/post/entity/Comment.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -12,18 +14,21 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Document(collection = "comments")
 public class Comment {
+    @Getter
     @Id
-    private String id;
+    private ObjectId id;
     private Long postId;
     private Long memberId;
     private String writer;
+    @Setter
     private String contents;
-    private String parentCommentId; // null이면 일반 댓글
+    private ObjectId parentCommentId; // null이면 일반 댓글
     private LocalDateTime createdAt;
+    @Setter
     private LocalDateTime updatedAt;
 
     @Builder
-    public Comment(Long memberId, Long postId, String writer, String parentCommentId, String contents) {
+    public Comment(Long memberId, Long postId, String writer, ObjectId parentCommentId, String contents) {
         this.postId = postId;
         this.memberId = memberId;
         this.writer = writer;
@@ -32,5 +37,4 @@ public class Comment {
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
-
 }
