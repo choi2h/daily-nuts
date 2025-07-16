eb05d8543b07f8dcebd67415f78dc1c2d9681d50
kimsongie12 <song3078@naver.com>
2025-07-16 15:20:52 +0900

    feat:댓글+대댓글 조회 수정 및 댓글 수정기능추가

--- daily-nuts-server/src/main/java/com/dailynuts/post/service/CommentServiceImpl.java
+++ daily-nuts-server/src/main/java/com/dailynuts/post/service/CommentServiceImpl.java
@@ -6,8 +6,10 @@ import com.dailynuts.post.entity.Comment;
 import com.dailynuts.post.repository.CommentRepository;
 import com.dailynuts.post.service.mapper.CommentMapper;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,7 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final CommentMapper commentMapper;
 
+    //댓글 등록
     @Override
     public CommentResponse createComment(Long postId, Long memberId, String writer, CommentRequest request) {
         validateContentLength(request.getContents());
@@ -30,9 +33,9 @@ public class CommentServiceImpl implements CommentService {
     }
 
 
-
+    //대댓글
     @Override
-    public CommentResponse createReplyToComment(Long postId, String parentCommentId, Long memberId, String writer, CommentRequest request) {
+    public CommentResponse createReplyToComment(Long postId, ObjectId parentCommentId, Long memberId, String writer, CommentRequest request) {
         validateContentLength(request.getContents());
 
         // 부모 댓글 유효성 확인
@@ -49,7 +52,7 @@ public class CommentServiceImpl implements CommentService {
         return commentMapper.toResponse(saved);
     }
 
-
+    //댓글+대댓글 조회
     @Override
     public List<CommentResponse> getCommentsByPostId(Long postId) {
         List<Comment> parentComments = commentRepository.findByPostIdAndParentCommentIdIsNullOrderByCreatedAtAsc(postId);
@@ -57,7 +60,9 @@ public class CommentServiceImpl implements CommentService {
         List<CommentResponse> responseList = new ArrayList<>();
 
         for (Comment parent : parentComments) {
+            System.out.println(">> 부모 댓글 ID = " + parent.getId());
             List<Comment> replies = commentRepository.findByParentCommentId(parent.getId());
+            System.out.println(">> 대댓글 수 = " + replies.size());
             CommentResponse parentResponse = commentMapper.toResponse(parent);
             List<CommentResponse> replyResponses = commentMapper.toResponseList(replies);
             parentResponse.setReplies(replyResponses);
@@ -71,6 +76,25 @@ public class CommentServiceImpl implements CommentService {
         if (contents == null || contents.length() < 1 || contents.length() > 100) {
             throw new IllegalArgumentException("댓글 내용은 1자 이상 100자 이하로 입력해주세요.");
         }
+    }
+
+    //댓글 수정
+    @Override
+    public CommentResponse updateComment(Long postId, String id, Long memberId, CommentRequest request){
+        Comment comment = commentRepository.findById(new ObjectId(id))
+                .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 댓글입니다."));
+
+        if(!comment.getMemberId().equals(memberId)){
+            throw  new IllegalArgumentException("댓글 작성자만 수정할 수 있습니다.");
+        }
+
+        validateContentLength(request.getContents());
+
+        comment.setContents(request.getContents());
+        comment.setUpdatedAt(LocalDateTime.now());
+        Comment updated = commentRepository.save(comment);
+
+        return commentMapper.toResponse(updated);
     }
 
 }
