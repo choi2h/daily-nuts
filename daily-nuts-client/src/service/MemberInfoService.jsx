da5a8aabb14c14b0764231e632a8d2c5e3548ee0
52kirai52 <52kirai52@gmail.com>
2025-07-18 14:53:55 +0900

    fix: cors에서 헤더 접근 허용 + loginhandler수정

--- daily-nuts-client/src/service/MemberInfoService.jsx
+++ daily-nuts-client/src/service/MemberInfoService.jsx
@@ -1,11 +1,8 @@
 import axios from "axios"
 
-const API_BASE_URL = import.meta.env.VITE_API_SERVER_ADDRESS;
-const LOGIN_URL = `${API_BASE_URL}/member/login`;
-
 const login = async (loginInfo) => {
-    return axios.post(LOGIN_URL, loginInfo, {
-        withCredentials: true       // ← 이걸 설정해야 쿠키가 자동으로 저장됨
+    return axios.post('/member/login', loginInfo, {
+       // ← 이걸 설정해야 쿠키가 자동으로 저장됨
         })
         .then((res) => {
             console.log(res);
