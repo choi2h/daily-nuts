72f4931089b66443cfe8eaf9d9a84833dfdbae69
kimsongie12 <song3078@naver.com>
2025-07-18 16:42:10 +0900

    feat: 게시글 없으면 댓글 생성 못하는 기능

--- daily-nuts-server/src/main/java/com/dailynuts/post/controller/CommentController.java
+++ daily-nuts-server/src/main/java/com/dailynuts/post/controller/CommentController.java
@@ -1,9 +1,12 @@
 package com.dailynuts.post.controller;
 
+import com.dailynuts.common.exception.CustomErrorCode;
+import com.dailynuts.common.exception.CustomException;
 import com.dailynuts.post.dto.CommentRequestDto;
 import com.dailynuts.post.dto.CommentResponseDto;
 import com.dailynuts.post.dto.CommentsResponseDto;
 import com.dailynuts.post.dto.DeleteResponseDto;
+import com.dailynuts.post.repository.PostRepository;
 import com.dailynuts.post.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/post/{postId}")
 public class CommentController {
     private final CommentService commentService;
+    private final PostRepository postRepository;
 
     //댓글 등록
     @PostMapping("/comment")
@@ -23,6 +27,9 @@ public class CommentController {
             @PathVariable Long postId,
             @RequestBody @Valid CommentRequestDto request
     ) {
+        if (!postRepository.existsById(postId)) {
+            throw new CustomException(CustomErrorCode.POST_NOT_FOUND);
+        }
         Long memberId = 1L; // 임시
         String writer = request.getWriter();
 
